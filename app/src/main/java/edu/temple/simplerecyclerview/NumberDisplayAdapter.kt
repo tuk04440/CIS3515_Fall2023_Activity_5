3b9e1fd0797554cd6e3175ec925c3b51aa04a723
Allen <tuk04440@temple.edu>
2024-02-15 11:59:01 -0500

    Completed step 1 for adapter to take in callback and implemented onClick behavior

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -9,12 +9,19 @@ import androidx.recyclerview.widget.RecyclerView
 // TODO(Step 1: Implement click behavior)
 
 // TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>, _callBack: (ImageIte)) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+class NumberDisplayAdapter (private val numbers: Array<Int>, _callBack: (Int)->Unit) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
     // TODO (Step 1b: Invoke lambda via onClickListener)
+    private val callback = _callBack
     inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
         // enumerate views inside layout
         val textView = layout.findViewById<TextView>(R.id.textView)
+
+        init {
+            textView.setOnClickListener{
+                callback(numbers[adapterPosition])
+            }
+        }
 
     }
 
